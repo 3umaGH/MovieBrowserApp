ac9ef60b584622948af8a40288d92bdc652abeae
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-17 20:33:08 +0200

    feat: sidebar now collapsible

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,10 @@
 import { useRef } from "react";
 import { useDraggable } from "react-use-draggable-scroll";
+import { useState } from "react";
 
-// Icons
+import { MdOutlineArrowForwardIos } from "react-icons/md";
+
+// placeholder icons
 import { FcDoughnutChart } from "react-icons/fc";
 import { BsAndroid } from "react-icons/bs";
 import { FcVip } from "react-icons/fc";
@@ -13,12 +16,40 @@ import { FcPrint } from "react-icons/fc";
 import { FcUnlock } from "react-icons/fc";
 
 export const Sidebar = () => {
+  const [isCollapsed, setCollapsed] = useState(false);
   const ref =
     useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
   const { events } = useDraggable(ref);
 
+  const toggleCollapsed = () => {
+    setCollapsed(!isCollapsed);
+  };
+
   return (
-    <div className="relative flex flex-col text-center top-0 w-32 h-screen bg-primary text-white shadow-md">
+    <div
+      className={`relative flex flex-col text-center top-0 w-${
+        isCollapsed ? "0" : "28"
+      } h-screen bg-primary text-white shadow-slate-800 shadow-2xl transition-all duration-500 ease-in-out`}
+    >
+      <div
+        className="absolute flex h-screen self-end w-4 left-full top-0 shadow-inner cursor-pointer"
+        onClick={toggleCollapsed}
+      ></div>
+      <div className="absolute flex bg-zinc-900 h-screen self-end w-1 top-0 shadow-inner"></div>
+
+      <div
+        className={`scale-${
+          isCollapsed ? "100" : "0"
+        } opacity-50 relative top-1/2 left-2 transition-all duration-300 cursor-pointer`}
+        onClick={toggleCollapsed}
+      >
+        <MdOutlineArrowForwardIos
+          className="cursor-pointer"
+          color="#474747"
+          size="30"
+        />
+      </div>
+
       <div
         className=" flex flex-col text-center items-center my-2 overflow-y-auto no-scrollbar"
         {...events}
