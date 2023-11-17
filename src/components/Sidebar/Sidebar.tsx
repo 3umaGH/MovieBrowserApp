d8f887851a7436d8d2eb6ad8f0bc09cd0e38bdc0
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-17 19:48:48 +0200

    added icon variation

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,51 +1,78 @@
 import { FcDoughnutChart } from "react-icons/fc";
+import { BsAndroid } from "react-icons/bs";
+
 import { FcVip } from "react-icons/fc";
 import { SidebarIcon } from "./SidebarIcon";
+import { FcInfo } from "react-icons/fc";
+import { FcCustomerSupport } from "react-icons/fc";
+import { FcCollaboration } from "react-icons/fc";
+import { FcPrint } from "react-icons/fc";
+import { FcUnlock } from "react-icons/fc";
 
 export const Sidebar = () => {
   return (
     <div className="relative flex flex-col text-center top-0 w-32 h-screen bg-primary text-white shadow-md">
       <div className=" flex flex-col text-center items-center my-2 overflow-y-auto no-scrollbar">
         <SidebarIcon
-          icon={<FcDoughnutChart size="48" />}
+          icon={<FcDoughnutChart size="40" />}
           tooltip="Piska popka 123"
         />
-        <SidebarIcon icon={<FcVip size="48" />} tooltip="piska small 32" />
+        <SidebarIcon icon={<FcVip size="40" />} tooltip="piska small 32" />
         <SidebarIcon
-          icon={<FcDoughnutChart size="48" />}
+          icon={<BsAndroid color="green" size="40" />}
           tooltip="Piska popka 123"
         />
-        <SidebarIcon icon={<FcVip size="48" />} tooltip="piska small 32" />
+        <SidebarIcon icon={<FcInfo size="40" />} tooltip="piska small 32" />
         <SidebarIcon
-          icon={<FcDoughnutChart size="48" />}
-          tooltip="Piska popka 123"
+          icon={<FcCustomerSupport size="40" />}
+          tooltip="piska small 32"
         />
-        <SidebarIcon icon={<FcVip size="48" />} tooltip="piska small 32" />
         <SidebarIcon
-          icon={<FcDoughnutChart size="48" />}
-          tooltip="Piska popka 123"
+          icon={<FcCollaboration size="40" />}
+          tooltip="piska small 32"
         />
-        <SidebarIcon icon={<FcVip size="48" />} tooltip="piska small 32" />
+        <SidebarIcon icon={<FcPrint size="40" />} tooltip="piska small 32" />
+        <SidebarIcon icon={<FcUnlock size="40" />} tooltip="piska small 32" />
         <SidebarIcon
-          icon={<FcDoughnutChart size="48" />}
+          icon={<FcDoughnutChart size="40" />}
           tooltip="Piska popka 123"
         />
-        <SidebarIcon icon={<FcVip size="48" />} tooltip="piska small 32" />
+        <SidebarIcon icon={<FcVip size="40" />} tooltip="piska small 32" />
         <SidebarIcon
-          icon={<FcDoughnutChart size="48" />}
+          icon={<BsAndroid color="green" size="40" />}
           tooltip="Piska popka 123"
         />
-        <SidebarIcon icon={<FcVip size="48" />} tooltip="piska small 32" />
+        <SidebarIcon icon={<FcInfo size="40" />} tooltip="piska small 32" />
         <SidebarIcon
-          icon={<FcDoughnutChart size="48" />}
-          tooltip="Piska popka 123"
+          icon={<FcCustomerSupport size="40" />}
+          tooltip="piska small 32"
         />
-        <SidebarIcon icon={<FcVip size="48" />} tooltip="piska small 32" />
         <SidebarIcon
-          icon={<FcDoughnutChart size="48" />}
+          icon={<FcCollaboration size="40" />}
+          tooltip="piska small 32"
+        />
+        <SidebarIcon icon={<FcPrint size="40" />} tooltip="piska small 32" />
+        <SidebarIcon icon={<FcUnlock size="40" />} tooltip="piska small 32" />
+        <SidebarIcon
+          icon={<FcDoughnutChart size="40" />}
           tooltip="Piska popka 123"
         />
-        <SidebarIcon icon={<FcVip size="48" />} tooltip="piska small 32" />
+        <SidebarIcon icon={<FcVip size="40" />} tooltip="piska small 32" />
+        <SidebarIcon
+          icon={<BsAndroid color="green" size="40" />}
+          tooltip="Piska popka 123"
+        />
+        <SidebarIcon icon={<FcInfo size="40" />} tooltip="piska small 32" />
+        <SidebarIcon
+          icon={<FcCustomerSupport size="40" />}
+          tooltip="piska small 32"
+        />
+        <SidebarIcon
+          icon={<FcCollaboration size="40" />}
+          tooltip="piska small 32"
+        />
+        <SidebarIcon icon={<FcPrint size="40" />} tooltip="piska small 32" />
+        <SidebarIcon icon={<FcUnlock size="40" />} tooltip="piska small 32" />
       </div>
     </div>
   );
