a9300fffd7f89d3e653664c71517a70d2d4718dd
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-18 21:40:54 +0200

    feat: implemented router

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,22 @@ import { fetchGenresThunk } from "./features/Parameters/thunks";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "./app/Store";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(fetchGenresThunk());
-  })
+  });
 
-  return <Main />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
