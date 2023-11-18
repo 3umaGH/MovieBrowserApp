1aa8fb0ab6140aff0dfb3592afa6ae7c5bea3465
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-18 17:11:31 +0200

    style: movie row dividers & text

--- src/components/MovieScroller/MovieRow.tsx
+++ src/components/MovieScroller/MovieRow.tsx
@@ -1,18 +1,33 @@
 import { useRef } from "react";
 import { useDraggable } from "react-use-draggable-scroll";
-import { useHorizontalScroll } from "../../hooks/useHorizontalScroll";
 
-export const MovieRow = ({ children }: { children: React.ReactNode }) => {
+export const MovieRow = ({
+  title,
+  children,
+}: {
+  title: String;
+  children: React.ReactNode;
+}) => {
   const ref =
     useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
   const { events } = useDraggable(ref);
   return (
-    <div
-      {...events}
-      ref={ref}
-      className="flex gap-14 m-2 mt-8 overflow-x-auto overflow-y-clip no-scrollbar h-min"
-    >
-      {children}
-    </div>
+    <>
+      <div className="mt-8">
+        <hr className=" mx-auto my-8 w-1/2  text-center h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
+
+        <h2 className="text-center font-sans text-5xl text-stone-200">
+          {title}
+        </h2>
+        <hr className=" mx-auto my-8 w-1/2  text-center h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
+      </div>
+      <div
+        {...events}
+        ref={ref}
+        className="flex gap-14 m-2 mt-8 overflow-x-auto overflow-y-clip no-scrollbar h-min"
+      >
+        {children}
+      </div>
+    </>
   );
 };
