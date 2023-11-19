a7ece05e84e64b70870fbb0de4f96fa41f950446
3umaGH <133604702+3umaGH@users.noreply.github.com>
2023-11-19 16:48:24 +0200

    feat: movie score component implemented

---
+++ src/components/MovieScore.tsx
@@ -0,0 +1,47 @@
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
+
+export const MovieScore = ({ score }: { score: number }) => {
+  return (
+    <div
+      className="w-full h-full rounded-full p-1"
+      style={{ backgroundColor: "#081C22" }}
+    >
+      <CircularProgressbarWithChildren
+        value={score}
+        maxValue={10}
+        styles={buildStyles({
+          pathColor:
+            score >= 7
+              ? "#21D07A" // Green
+              : score >= 5
+              ? "#F5A623" // Orange
+              : "#DB2360", // Red
+          trailColor:
+            score >= 7
+              ? "#204529" // Green
+              : score >= 5
+              ? "#6b480d" // Orange
+              : "#571435", // Red
+          textColor: "#242424",
+          textSize: "24px",
+        })}
+      >
+        <span
+          className="font-roboto font-semibold text-center mt-1"
+          style={{ fontSize: 29 }}
+        >
+          {((score / 10) * 100).toFixed(0)}
+          <span
+            className=" font-roboto align-text-top"
+            style={{ fontSize: 14 }}
+          >
+            %
+          </span>
+        </span>
+      </CircularProgressbarWithChildren>
+    </div>
+  );
+};
